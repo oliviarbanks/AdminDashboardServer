08dccc04f8e7b691e26ce2cd5957fc41d575e506
Olivia <oliviarbanks@gmail.com>
2023-09-18 00:59:11 -0400

    backend working on mysql and connected to front end. post only

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// const cookieParser = require ('cookie-parser');
+// const bcrypt = require ('bcrypt');
+// const jwt = require ('jsonwebtoken')
 const express = require ('express');
 const app = express();
 const mysql = require('mysql')
 const cors = require('cors');
 
-
 app.use(cors());
 app.use(express.json());
+// app.use(cookieParser());
 
 
 const db = mysql.createConnection({
@@ -35,6 +38,57 @@ app.post('/create', (req, res) => {
     )
 
 })
+
+
+// app.post('/login', (req, res) => {
+//     console.log(req.body);
+//     const email = req.body.email
+//     const password = req.body.password
+
+//     db.query('INSERT INTO `users` (email, password) VALUES (?,?)',
+//     [email, password], 
+//     (err, result) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         else {
+//             res.send("values inserted")
+//         }
+//     }
+//     )
+
+// })
+
+
+
+
+// app.post('/login', (req, res) => {
+//     const sql = "SELECT * FROM users Where email = ? AND password =?";
+//     db.query(sql, [req.body.email, req.body.password], (err, result) => {
+//         if (err) return res.json({Status: "Error", Error: "Error in running query"});
+//         if(result.length >0) {
+//             return res.json({Status: "Success"})
+//         } else {
+//             return res.json({Status: "Error", Error: "Wrong Email or Password"})
+//         }
+//     })
+//     console.log(req.body);
+//     const email = req.body.email
+//     const password = req.body.password
+
+//     db.query('INSERT INTO `users` (email, password) VALUES (?,?)',
+//     [email, password], 
+//     (err, result) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         else {
+//             res.send("values inserted")
+//         }
+//     }
+//     )
+
+// }
 
 
 
